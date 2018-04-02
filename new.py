ef8e661a032471ce6f72042ff51613a49519fac0
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2018-04-02 13:02:45 -0700

    New style for the front-page

--- new.py
+++ new.py
@@ -41,7 +41,7 @@ def createJsFile(challenge):
 
 # 4 - Add the new challenge to index.pug
 def addChallenge(challenge):
-    challenge_import = "\t\tbr\n\t\ta(href='/" + challenge + "/') " + challenge
+    challenge_import = "\t\t\tli #[a(href='/" + challenge + "/') " + challenge + "]"
     index = "src/index.pug"
     if os.stat(index).st_size == 0:
         open_global = open(index, 'a')
